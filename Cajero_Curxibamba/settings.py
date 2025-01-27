9c359efc1da079057a635f8b425341c0ee5c1069
luis-blacio <luis.blacio@unl.edu.ec>
2025-01-27 07:42:46 -0500

    admin y models actualizados

--- Cajero_Curxibamba/settings.py
+++ Cajero_Curxibamba/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'busqueda_avanzada.apps.BusquedaAvanzadaConfig'
+    'busqueda_avanzada'
 ]
 
 MIDDLEWARE = [
